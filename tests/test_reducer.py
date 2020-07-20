ac22e88bb11e8544d8ff8afb970e5c86b3c4a8b0
Matthieu <matthieu@Matthieus-MacBook-Pro.local>
2020-07-20 22:05:17 +0200

    reducer moved to unittest and subtest

--- tests/test_reducer.py
+++ tests/test_reducer.py
@@ -1,7 +1,8 @@
+import unittest
+
 import json
 import os
 
-from nose.tools import assert_raises, assert_equal, assert_true
 from craft_ai import reduce_decision_rules, errors
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -10,42 +11,36 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 # constructed
 EXPECTATIONS_DIR = os.path.join(HERE, "data", "interpreter", "reduce_decision_rules")
 
+class TestReducer(unittest.TestCase):
 
-def reduce_decision_rules_tests_generator():
-    expectations_files = os.listdir(EXPECTATIONS_DIR)
-    for expectations_file in expectations_files:
-        if os.path.splitext(expectations_file)[1] == ".json":
-            with open(os.path.join(EXPECTATIONS_DIR, expectations_file)) as f:
-                expectations = json.load(f)
+    def test_reduce_decision_rules_tests_generator(self):
+        expectations_files = os.listdir(EXPECTATIONS_DIR)
+        for expectations_file in expectations_files:
+            if os.path.splitext(expectations_file)[1] == ".json":
+                with open(os.path.join(EXPECTATIONS_DIR, expectations_file)) as f:
+                    expectations = json.load(f)
 
-            for expectation in expectations:
-                assert_true(
-                    "title" in expectation,
-                    "Invalid expectation from '{}': missing \"title\".".format(
-                        expectations_file
-                    ),
-                )
-                assert_true(
-                    "rules" in expectation and "expectation" in expectation,
-                    'Invalid expectation from \'{}\': missing "rules" or "expectation".'.format(
-                        expectations_file
-                    ),
-                )
+                for expectation in expectations:
+                    self.assertTrue(
+                        "title" in expectation,
+                        "Invalid expectation from '{}': missing \"title\".".format(
+                            expectations_file
+                        ),
+                    )
+                    self.assertTrue(
+                        "rules" in expectation and "expectation" in expectation,
+                        'Invalid expectation from \'{}\': missing "rules" or "expectation".'.format(
+                            expectations_file
+                        ),
+                    )
 
-                def test_fn(r, e):
-                    return check_expectation(r, e)
+                    for expectation in expectations:
+                        with self.subTest():
+                            self.check_expectation(expectation["rules"], expectation["expectation"])
+                        
+    def check_expectation(self, rules, expectation):
 
-                test_fn.description = expectation["title"]
-                reduce_decision_rules_tests_generator.compat_func_name = (
-                    test_fn.description
-                )
-
-                yield test_fn, expectation["rules"], expectation["expectation"]
-
-
-def check_expectation(rules, expectation):
-
-    if "error" in expectation:
-        assert_raises(errors.CraftAiError, reduce_decision_rules, rules)
-    else:
-        assert_equal(reduce_decision_rules(rules), expectation["rules"])
+        if "error" in expectation:
+            self.assertRaises(errors.CraftAiError, reduce_decision_rules, rules)
+        else:
+            self.assertEqual(reduce_decision_rules(rules), expectation["rules"])
