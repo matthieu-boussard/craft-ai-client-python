7be9ea05d5fc53eaf3bd0557a9230ef5997153ac
Matthieu <matthieu@Matthieus-MacBook-Pro.local>
2020-07-20 21:36:24 +0200

    test formatters uses subtest from unittest instead of nose and generators

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -1,85 +1,82 @@
+import unittest
+
 import json
 import os
 
 from dateutil.parser import isoparse
-from nose.tools import assert_equal, assert_true, assert_raises
 
 from craft_ai import format_property, format_decision_rules, errors
 
+class TestFormatter(unittest.TestCase):
 
-def test_format_property_time_of_day():
-    formatter = format_property("time_of_day")
+    def test_format_property_time_of_day(self):
+        formatter = format_property("time_of_day")
 
-    assert_equal(formatter(11.5), "11:30")
-    assert_equal(formatter(11.008), "11:00:28")
+        self.assertEqual(formatter(11.5), "11:30")
+        self.assertEqual(formatter(11.008), "11:00:28")
 
-    assert_equal(formatter(isoparse("2016-10-20T08:20:03")), "08:20:03")
-    assert_equal(formatter(isoparse("2016-08-12T13:37")), "13:37")
-
-
-def test_format_property_enum():
-    formatter = format_property("enum")
-
-    assert_equal(formatter("toto"), "toto")
+        self.assertEqual(formatter(isoparse("2016-10-20T08:20:03")), "08:20:03")
+        self.assertEqual(formatter(isoparse("2016-08-12T13:37")), "13:37")
 
 
-def test_format_property_continuous():
-    formatter = format_property("continuous")
+    def test_format_property_enum(self):
+        formatter = format_property("enum")
 
-    assert_equal(formatter(12.4), "12.4")
-    assert_equal(formatter(12.4234), "12.42")
-
-
-def test_format_property_month_of_year():
-    formatter = format_property("month_of_year")
-
-    assert_equal(formatter(1), "Jan")
-    assert_equal(formatter(6), "Jun")
-    assert_equal(formatter(12), "Dec")
+        self.assertEqual(formatter("toto"), "toto")
 
 
-HERE = os.path.abspath(os.path.dirname(__file__))
+    def test_format_property_continuous(self):
+        formatter = format_property("continuous")
 
-# Assuming we are the test folder and the folder hierarchy is correctly
-# constructed
-EXPECTATIONS_DIR = os.path.join(HERE, "data", "interpreter", "format_decision_rules")
-
-
-def format_decision_rule_tests_generator():
-    expectations_files = os.listdir(EXPECTATIONS_DIR)
-    for expectations_file in expectations_files:
-        if os.path.splitext(expectations_file)[1] == ".json":
-            # Loading the expectations for this tree
-            with open(os.path.join(EXPECTATIONS_DIR, expectations_file)) as f:
-                expectations = json.load(f)
-
-            for expectation in expectations:
-                assert_true(
-                    "title" in expectation,
-                    "Invalid expectation from '{}': missing \"title\".".format(
-                        expectations_file
-                    ),
-                )
-                assert_true(
-                    "rules" in expectation and "expectation" in expectation,
-                    'Invalid expectation from \'{}\': missing "rules" or "expectation".'.format(
-                        expectations_file
-                    ),
-                )
-
-                def test_fn(r, e):
-                    return check_expectation(r, e)
-
-                test_fn.description = expectation["title"]
-                format_decision_rule_tests_generator.compat_func_name = (
-                    test_fn.description
-                )
-
-                yield test_fn, expectation["rules"], expectation["expectation"]
+        self.assertEqual(formatter(12.4), "12.4")
+        self.assertEqual(formatter(12.4234), "12.42")
 
 
-def check_expectation(rules, expectation):
-    if "error" in expectation:
-        assert_raises(errors.CraftAiError, format_decision_rules, rules)
-    else:
-        assert_equal(format_decision_rules(rules), expectation["string"])
+    def test_format_property_month_of_year(self):
+        formatter = format_property("month_of_year")
+
+        self.assertEqual(formatter(1), "Jan")
+        self.assertEqual(formatter(6), "Jun")
+        self.assertEqual(formatter(12), "Dec")
+
+
+class TestFormatDecisionRules(unittest.TestCase):
+
+    def test_format_decision_rule_(self):
+
+        HERE = os.path.abspath(os.path.dirname(__file__))
+
+        # Assuming we are the test folder and the folder hierarchy is correctly
+        # constructed
+        EXPECTATIONS_DIR = os.path.join(HERE, "data", "interpreter", "format_decision_rules")
+
+        expectations_files = os.listdir(EXPECTATIONS_DIR)
+        for expectations_file in expectations_files:
+            if os.path.splitext(expectations_file)[1] == ".json":
+                # Loading the expectations for this tree
+                with open(os.path.join(EXPECTATIONS_DIR, expectations_file)) as f:
+                    expectations = json.load(f)
+
+                for expectation in expectations:
+                    self.assertTrue(
+                        "title" in expectation,
+                        "Invalid expectation from '{}': missing \"title\".".format(
+                            expectations_file
+                        ),
+                    )
+                    self.assertTrue(
+                        "rules" in expectation and "expectation" in expectation,
+                        'Invalid expectation from \'{}\': missing "rules" or "expectation".'.format(
+                            expectations_file
+                        ),
+                    )
+
+                    for expectation in expectations:
+                        with self.subTest():
+                            self.check_expectation(expectation["rules"], expectation["expectation"])
+
+    def check_expectation(self, rules, expectation):
+        if "error" in expectation:
+            self.assertRaises(errors.CraftAiError, format_decision_rules, rules)
+        else:
+            self.assertEqual(format_decision_rules(rules), expectation["string"])
