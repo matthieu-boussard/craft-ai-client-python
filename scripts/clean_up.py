304ca04c4666ae1e8e338ed641830e0c85c4a292
Gaetan Millerand <>
2020-10-15 17:42:52 +0200

    Add cleaning to the generator part

--- scripts/clean_up.py
+++ scripts/clean_up.py
@@ -7,8 +7,13 @@ load_dotenv(find_dotenv())
 CRAFT_CFG = {"token": os.environ.get("CRAFT_TOKEN")}
 JOB_ID = os.environ.get("TRAVIS_JOB_ID", "loc")
 
+print("Cleaning dangling agents and generators from test session.")
 craft_client = craft_ai.Client(CRAFT_CFG)
 agents_list = craft_client.list_agents()
 agents_to_delete = [agent_id for agent_id in agents_list if agent_id[-3:] == JOB_ID[-3:]]
 for agent_id in agents_to_delete:
   craft_client.delete_agent(agent_id)
+generators_list = craft_client.list_generators()
+generators_to_delete = [generator_id for generator_id in generators_list if generator_id[-3:] == JOB_ID[-3:]]
+for generator_id in generators_to_delete:
+  craft_client.delete_generator(generator_id)
